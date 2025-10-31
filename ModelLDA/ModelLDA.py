b781485f84abee1a2c41b6bd4dd3c3fa5e40c2f4
IbnuSyifa <119270543+IbnuSyifa@users.noreply.github.com>
2025-10-31 15:38:56 +0000

    deleting gensim

--- ModelLDA/ModelLDA.py
+++ ModelLDA/ModelLDA.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import gensim
+
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from gensim.corpora import Dictionary
